fa618f9fa96d858a0c7ef1025b55a124e34c23dc
Németh Palkó <nemeth.palko@hotmail.com>
2020-01-03 16:47:17 +0100

    asd

---
+++ 2.óra/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace _2.óra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string[] raktarkeszlet;
+            raktarkeszlet = File.ReadAllLines(@"C:\Users\nemet\source\repos\szellgergo\Tutorial\Segedanyagok\Raktarkeszlet.txt");
+        }
+    }
+}
