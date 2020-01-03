4099d0468aec1b3d852ba2a55c1e8aaef705232d
SGergo <SGergo@WTG-L085>
2020-01-03 17:20:03 +0100

    Feladat1

--- 2.óra/Program.cs
+++ 2.óra/Program.cs
@@ -6,13 +6,22 @@ namespace _2.óra
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Beolvasas();
+            List<Termek> raktarlista = new List<Termek>();
+            raktarlista = Beolvasas();
+            
+            Feladat1(raktarlista);
             
         }
 
-        private static void Beolvasas()
+        private static void Feladat1(List<Termek> raktarlista)
+        {
+            
+        }
+
+        private static List<Termek> Beolvasas()
         {
             List<Termek> raktarlista = new List<Termek>();
             List<string> raktarlista2 = new List<string>();
@@ -26,9 +35,9 @@ namespace _2.óra
                     Console.WriteLine(tomb[j]);
                     raktarlista.Add(new Termek {nev=tomb[0],vonalkod=Int32.Parse(tomb[1]),mennyiseg=Int32.Parse(tomb[2]),afa=Int32.Parse(tomb[3].Split(',')[0]), netto=double.Parse(tomb[4])});
                     raktarlista2.Add(tomb[j]);
-
                 }
             }
+            return raktarlista;
         }
     }
 }
